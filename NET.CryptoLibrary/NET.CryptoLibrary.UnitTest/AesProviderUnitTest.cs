2e62bdfb8ca4072653d7840d0ba46ee72b535fb9
nocommentlab <blescia.antonio@gmail.com>
2020-11-10 22:50:51 +0100

    First Commit

---
+++ NET.CryptoLibrary/NET.CryptoLibrary.UnitTest/AesProviderUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ncl.net.cryptolybrary.Encryption.AES;
+
+namespace NET.CryptoLibrary.UnitTest
+{
+    [TestClass]
+    public class AesProviderUnitTest
+    {
+        [TestMethod]
+        public void EncryptStringTest()
+        {
+            string STRING_EncryptedString = AesProvider.AES_CBC_Encryption_Rand_IV("0123456789qwertyuiopasdfghjklzxcvbnm", "mykey");       
+            Assert.IsTrue(STRING_EncryptedString != null && STRING_EncryptedString.Length > 0);
+        }
+
+        [TestMethod]
+        public void EncryptBufferTest()
+        {
+            byte[] vBYTE_EncryptedBuffer = AesProvider.AES_CBC_Encryption_Rand_IV(new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 }, "mykey");
+            Assert.IsNotNull(vBYTE_EncryptedBuffer);
+        }
+
+        [TestMethod]
+        public void EncryptDecryptStringTest()
+        {
+            string STRING_EncryptionKey = "thisIsMyP@55w0rd";
+            string STRING_ToEncrypt = "0123456789qwertyuiopasdfghjklzxcvbnm";
+            string STRING_EncryptedString = AesProvider.AES_CBC_Encryption_Rand_IV(STRING_ToEncrypt, STRING_EncryptionKey);
+            string STRING_DecryptedString = AesProvider.AES_CBC_Decryption_Rand_IV(STRING_EncryptedString, STRING_EncryptionKey);
+            Assert.IsTrue(STRING_DecryptedString.Equals(STRING_ToEncrypt));
+        }
+
+        [TestMethod]
+        public void EncryptDecryptBufferTest()
+        {
+            string STRING_EncryptionKey = "thisIsMyP@55w0rd";
+            byte[] vBYTE_ToEncrypt = Encoding.UTF8.GetBytes("0123456789qwertyuiopasdfghjklzxcvbnm");//new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
+            byte[] vBYTE_EncryptedBuffer = AesProvider.AES_CBC_Encryption_Rand_IV(vBYTE_ToEncrypt, STRING_EncryptionKey);
+            byte[] vBYTE_DecryptedBuffer = AesProvider.AES_CBC_Decryption_Rand_IV(vBYTE_EncryptedBuffer, STRING_EncryptionKey);
+            Assert.IsTrue(vBYTE_ToEncrypt.SequenceEqual(vBYTE_DecryptedBuffer));
+        }
+    }
+}
---
+++ NET.CryptoLibrary/NET.CryptoLibrary.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NET.CryptoLibrary.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NET.CryptoLibrary.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("60e0e54d-b6a4-4de7-9fad-64066802161c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
